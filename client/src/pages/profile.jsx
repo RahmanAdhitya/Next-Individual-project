b4e83e0a244bd7fcf891985171a338e66b54fdd4
RahmanAdhitya <rahman.adhitya23@gmail.com>
2022-04-10 00:49:43 +0700

    same point with last commit

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -25,25 +25,51 @@ import {
   useEditableControls,
 } from '@chakra-ui/react';
 import { useFormik } from 'formik';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import axiosInstance from '../lib/api';
-import { Icon } from '@chakra-ui/react';
-import { FiEdit } from 'react-icons/fi';
-import { BsCheck2Square } from 'react-icons/bs';
+import { auth_types, network_types } from '../redux/types';
 
 const Profile = () => {
   const authSelector = useSelector((state) => state.auth);
+  const inputFileRef = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(null);
   const dispatch = useDispatch();
 
   const [edit, setEdit] = useState();
 
-  const formik = useFormik({});
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      bio: '',
+    },
+  });
 
   const inputHandler = (event) => {
     const { value, name } = event.target;
 
     formik.setFieldValue(name, value);
+  };
+
+  const handelFile = (event) => {
+    setSelectedFile(event.target.files[0]);
+    console.log(event.target.files[0]);
+  };
+
+  const uploadContentHandler = async () => {
+    const formData = new FormData();
+    const { username, bio } = formik.values;
+
+    formData.append('username', username);
+    formData.append('bio', bio);
+    formData.append('profile_image_file', selectedFile);
+    console.log(formData.values);
+    await axiosInstance.patch('/auth/profile', formData);
+
+    dispatch({
+      type: auth_types.LOGIN_USER,
+      payload: formData,
+    });
   };
 
   useEffect(() => {
@@ -58,19 +84,31 @@ const Profile = () => {
           <Heading mt={5} ms={4} lineHeight={1.1} fontSize={{ base: 'xl', sm: '2xl' }} justifyContent="space-between">
             User Profile
           </Heading>
-          <Button size="sm" onClick={() => setEdit(!edit)} mt={5} me={4}>
-            Edit
-          </Button>
+          {edit ? (
+            <Button size="sm" mt={5} me={4} onClick={() => setEdit(!edit)} colorScheme="orange">
+              cancel
+            </Button>
+          ) : (
+            <Button size="sm" mt={5} me={4} onClick={() => setEdit(!edit)} colorScheme="messenger">
+              edit
+            </Button>
+          )}
         </Flex>
         <Flex mt={5} justify={'center'} spacing={2}>
           <Avatar size="xl" src={authSelector.image_url}></Avatar>
+          <Input accept="image/png, image/jpeg" display="none" type="file" id="image_url" placeholder="Please enter domain" name="image_url" onChange={handelFile} ref={inputFileRef} />
+        </Flex>
+        <Flex mt={5} justify={'center'} spacing={2}>
+          <Button size="sm" hidden={edit ? false : true} onClick={() => inputFileRef.current.click()} colorScheme="facebook">
+            choose File
+          </Button>
         </Flex>
 
         <Box p={6}>
           <Stack spacing={2}>
             <FormControl>
               <FormLabel>Username</FormLabel>
-              <Input name="username" defaultValue={authSelector.username} isDisabled={edit ? false : true} />
+              <Input id="username" name="username" defaultValue={authSelector.username} isDisabled={edit ? false : true} onChange={inputHandler} />
 
               <FormLabel mt={4}>Full Name</FormLabel>
               <Input defaultValue={authSelector.fullName} isDisabled />
@@ -79,10 +117,16 @@ const Profile = () => {
               <Input defaultValue={authSelector.email} isDisabled />
 
               <FormLabel mt={4}>Bio</FormLabel>
-              <Textarea name="bio" defaultValue={authSelector.bio} isDisabled={edit ? false : true} />
+              <Textarea id="bio" name="bio" defaultValue={authSelector.bio} isDisabled={edit ? false : true} onChange={inputHandler} />
             </FormControl>
           </Stack>
         </Box>
+
+        <Flex mb={5} me={5} justify={'end'} spacing={2}>
+          <Button size="sm" hidden={edit ? false : true} onClick={uploadContentHandler} colorScheme="facebook">
+            Submit
+          </Button>
+        </Flex>
       </Box>
     </Flex>
   );
